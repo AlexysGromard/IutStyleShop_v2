1a0572a3d5f3d913635e6c90445cafb2a8645bc3
Lancelot JOUAULT <E226125N@ubuntu.u-inf-j-c002-14>
2023-12-20 12:22:08 +0100

    ajout des table pres config dans test

---
+++ database/Test/second_table.sql
@@ -0,0 +1,17 @@
+-- Exemple d'insertion dans la table Accesoire
+INSERT INTO Article (idArticle, quantite)
+VALUES 
+(5,50),
+(8,1),
+(10,10000);
+
+-- Exemple d'insertion dans la table Accesoire
+INSERT INTO Vetement (idArticle, quantiteXS ,quantiteS ,quantiteM, quantiteL,quantiteXL)
+VALUES 
+(1,10,65,4989,102,164),
+(2,5,0,465,7989,30),
+(3,73878,1564,2574,7354,4345),
+(4,0,4646,0,454,0),
+(6,0,0,0,0,0),
+(7,1,786,78676,72785,735524),
+(9,738753,0,4978789,3,4532);
--- database/tables/User.sql
+++ database/tables/User.sql
@@ -7,11 +7,14 @@ CREATE OR REPLACE TABLE User (
 	password VARCHAR(255) NOT NULL, -- crypter avec bcrypt
 	nom VARCHAR(64) NULL,
 	prenom  VARCHAR(64) NULL,
+	role VARCHAR(16) NOT NULL,
 	adresse VARCHAR2(255) NULL,
 	ville VARCHAR(128) NULL,
 	code_postal MEDIUMINT NULL,
 	Complement_adresse VARCHAR2(128) NULL,
 );
 
-
+INSERT INTO User (email, password, nom, prenom, adresse, ville, code_postal, Complement_adresse)
+VALUES 
+('admin@iutstyleshop.com', 'admin', NULL, NULL, 'admin', NULL, NULL , NULL, NULL);
 
