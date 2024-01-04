db4da3ff8265636d76c37872ff0a84419564876d
XIV <lancelot.jouault@outlook.com>
2024-01-04 16:09:15 +0100

    correction

--- database/tables/ArticleCommande.sql
+++ database/tables/ArticleCommande.sql
@@ -1,10 +1,9 @@
 --Table Ticket
-CREATE OR REPLACE TABLE ArticleComande(
+CREATE OR REPLACE TABLE ArticleCommande(
     id_commande INT PRIMARY KEY  NOT NULL,
     id_Article INT PRIMARY KEY  NOT NULL,
     taille VARCHAR(4) NOT NULL,
     prix_unitaire FLOAT NOT NULL,	-- ne peut pas etre negatif
-    promo TINYINT NOT NULL, -- ne peut pas etre < 0 ou >100
     quantite TINYINT NOT NULL ,  -- ne peut pas etre a 0 ou >100
 FOREIGN KEY (id_commande) REFERENCES Commande(id),
 FOREIGN KEY (id_Article) REFERENCES Article (id)
