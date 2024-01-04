03e9629213ebe6d9961f6d04b7afae835f393f0a
XIV <lancelot.jouault@outlook.com>
2024-01-04 16:08:18 +0100

    Fichier Test a fait mais non tester

--- database/tables/Commande.sql
+++ database/tables/Commande.sql
@@ -2,8 +2,8 @@
 CREATE OR REPLACE TABLE Commande(
     id INT NOT NULL  AUTO_INCREMENT PRIMARY KEY,
     id_user INT NOT NULL,
-    date DATETIME NOT NULL,
+    date DATETIME DEFAULT NOW() NOT NULL,
     statut VARCHAR(16) NOT NULL,
-    prix FLOAT NOT NULL,	 -- ne peut pas etre negatif
+    prix FLOAT NULL,	 -- ne peut pas etre negatif
 FOREIGN KEY (id_user) REFERENCES User(id)
 );
