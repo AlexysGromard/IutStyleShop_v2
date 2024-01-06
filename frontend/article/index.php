7ed9eaace884f7781108f485c34a1ba37dee6fe2
E226772R <alexys.gromard@etu.univ-nantes.fr>
2024-01-06 17:43:35 +0100

    Correction mineure

--- frontend/article/index.php
+++ frontend/article/index.php
@@ -5,9 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Article - IUTStyleShop</title>
-    <link rel="stylesheet" href="/frontend/styles/variables.css">
-    <link rel="stylesheet" href="/frontend/styles/general.css">
-    <link rel="stylesheet" href="/frontend/styles/homePage.css">
+    <?php include 'frontend/common-includes.php'?>
     <link rel="stylesheet" href="/frontend/styles/article.css">
 </head>
 <body>
