a924121da7eff3920ffd0c53c9cc8c9b31673397
E226772R <alexys.gromard@etu.univ-nantes.fr>
2024-01-08 10:12:57 +0100

    Config

---
+++ backend/config/config.php
@@ -0,0 +1,10 @@
+<?php
+    const DB_CONFIG = array(
+        "dbname" => "db_name",
+        "username" => "db_username",
+        "password" => "db_password",
+        "host" => "db_host",
+        "type" => "type",
+        "port" => 0000 
+    );
+?>
