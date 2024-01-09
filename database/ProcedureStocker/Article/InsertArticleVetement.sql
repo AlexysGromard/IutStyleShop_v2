ac05f8520cff29f87e33a523bdbc2ec259111be6
Lancelot JOUAULT <E226125N@ubuntu.u-inf-j-c002-14>
2024-01-09 17:10:18 +0100

    correction

--- database/ProcedureStocker/Article/InsertArticleVetement.sql
+++ database/ProcedureStocker/Article/InsertArticleVetement.sql
@@ -23,9 +23,9 @@ DELIMITER //
         -- Utilisez l'auto-incrémenté pour obtenir l'ID 
         -- Ajoutez des validations si nécessaire
         -- (Vérification de la plage de promo, etc.)
-        SET @last_id = SELECT LAST_INSERT_ID() ;
+        SET @last_id = LAST_INSERT_ID() ;
         INSERT INTO Article (nom, category, genre, couleur, description, prix, promo, disponible) VALUES (p_nom, p_category, p_genre, p_couleur, p_description, p_prix, p_promo, p_disponible);
-        SET @last_id_2 = SELECT LAST_INSERT_ID() ;
+        SET @last_id_2 = LAST_INSERT_ID() ;       
         IF last_id_2 >= last_id + 2 THEN
             SIGNAL SQLSTATE '45151' SET MESSAGE_TEXT = 'Deux articles ont été ajouté en même temps !';
         ELSE
--- database/commande.bash
+++ database/commande.bash
@@ -12,62 +12,63 @@ host='localhost'
 database='DB_IutStyleShop'
 
 # Database
-echo ""
-echo " \e[35m Exécution des fichiers Database : "
-echo "\e[34m* \e[0m $database \e[31m"
+echo -e ""
+echo -e " \e[35m Exécution des fichiers Database : "
+echo -e "    \e[34m * \e[0m $database \e[31m"
 sudo mysql --host=$host --user=$user --database="" --password=$password < "mariadb/database.sql"
 
 
-
+    
+    
 # Table
-echo ""
-echo "\e[35m Exécution des fichiers Tables : "
-echo "\e[34m* \e[0m User \e[31m"
+echo -e ""
+echo -e "\e[35m Exécution des fichiers Tables : "
+echo -e "    \e[34m * \e[0m User \e[31m"
 sudo mysql --host=$host --user=$user --database=$database --password=$password < "tables/User.sql"
 
-echo "\e[34m* \e[0m Article \e[31m"
+echo -e "    \e[34m * \e[0m Article \e[31m"
 sudo mysql --host=$host --user=$user --database=$database --password=$password < "tables/Article.sql"
-echo "\e[34m* \e[0m Image \e[31m"
+echo -e "    \e[34m * \e[0m Image \e[31m"
 sudo mysql --host=$host --user=$user --database=$database --password=$password < "tables/Image.sql"
-echo "\e[34m* \e[0m Accesoire \e[31m"
+echo -e "    \e[34m * \e[0m Accesoire \e[31m"
 sudo mysql --host=$host --user=$user --database=$database --password=$password < "tables/Accesoire.sql"
-echo "\e[34m* \e[0m Vetement \e[31m"
+echo -e "    \e[34m * \e[0m Vetement \e[31m"
 sudo mysql --host=$host --user=$user --database=$database --password=$password < "tables/Vetement.sql"
 
-echo "\e[34m* \e[0m Panier \e[31m"
+echo -e "    \e[34m * \e[0m Panier \e[31m"
 sudo mysql --host=$host --user=$user --database=$database --password=$password < "tables/Panier.sql"
 
-echo "\e[34m* \e[0m Commentaire \e[31m"
+echo -e "    \e[34m * \e[0m Commentaire \e[31m"
 sudo mysql --host=$host --user=$user --database=$database --password=$password < "tables/Commentaire.sql"
 
-echo "\e[34m* \e[0m Commande \e[31m"
+echo -e "    \e[34m * \e[0m Commande \e[31m"
 sudo mysql --host=$host --user=$user --database=$database --password=$password < "tables/Commande.sql"
-echo "\e[34m* \e[0m ArticleCommande \e[31m"
+echo -e "    \e[34m * \e[0m ArticleCommande \e[31m"
 sudo mysql --host=$host --user=$user --database=$database --password=$password < "tables/ArticleCommande.sql"
 
-echo "\e[34m* \e[0m CodePromo \e[31m"
+echo -e "    \e[34m * \e[0m CodePromo \e[31m"
 
 # Trigger
-echo ""
-echo "\e[35m Exécution des fichiers Trigger : "
+echo -e ""
+echo -e "\e[35m Exécution des fichiers Trigger : "
 dossier="Triggers"
 find "$dossier" -type f -name "*.sql" | while read -r fichier; do
-    echo "\e[34m* \e[0m $fichier \e[31m"
+    echo -e "    \e[34m * \e[0m $fichier \e[31m"
     sudo mysql --host=$host --user=$user --database=$database --password=$password < "$fichier"
 done
 
 # Package
-# echo ""
-# echo "Exécution des fichiers Package :"
-# dossier="Packages"
+# echo -e ""
+# echo -e "\e[35m Exécution des fichiers Package :      \e[31m"
+# dossier="Packages "
 # find "$dossier" -type f -name "*.sql" | while read -r fichier; do
-#     echo "* $fichier"
+#     echo -e "\e[34m * \e[0m * $fichier"
 #     sudo mysql --host=$host --user=$user --database=$database --password=$password < "$fichier"
 # done
 
 #Procedure Stockée
-echo ""
-echo "\e[35m Exécution des fichiers Procedure Stockée : "
+echo -e ""
+echo -e "\e[35m Exécution des fichiers Procedure Stockée : "
 dossier="ProcedureStocker"
 # ProcedureStocker/
 # ├── Article
@@ -86,7 +87,7 @@ dossier="ProcedureStocker"
 # └── User
 
 find "$dossier" -type f -name "*.sql" | while read -r fichier; do
-    echo "\e[34m* \e[0m $fichier \e[31m"
+    echo -e "    \e[34m * \e[0m $fichier \e[31m"
     sudo mysql --host=$host --user=$user --database=$database --password=$password < "$fichier"
 done
 
