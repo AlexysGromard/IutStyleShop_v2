713cfaf4a8aa7d9698dfc1b5b8e03ba0ed9200a6
Floran MARTEL <E226752U@ubuntu.srv-podman-2>
2024-01-10 18:04:31 +0100

    updat

--- backend/DAO/DBPanier.php
+++ backend/DAO/DBPanier.php
@@ -2,46 +2,74 @@
 
 namespace backend\DAO;
 
+use Override;
+
 class DBPanier extends Connexion implements DAOInterface
 {
-    public function add(PanierEntity $entity)
+    public static function add($entity,$user) //\backend\entity\PanierEntity 
     {   
-        $requete = "call PanierPackage.InsertPanier(:idArticle, :idUser, :taille, :quantite)";
-        $stmt = $this->pdo->prepare($requete);
-        $stmt->execute([
-            'idArticle' => $entity->getIdArticle(),
-            'idUser' => $entity->getIdUser(),
-            'taille' => $entity->getTaille(),
-            'quantite' => $entity->getQuantite()
-        ]);
+        try {
+            $requete = "CALL InsertPanier(?,?,?,?)";
+            $stmt = self::$pdo->prepare($requete);
+
+            // Lie les paramètres d'entrée
+            $stmt->bindParam(1, $entity->id_article, \PDO::PARAM_INT);
+            $stmt->bindParam(1, $entity->taille, \PDO::PARAM_INT);
+            $stmt->bindParam(1, $entity->taille, \PDO::PARAM_INT);
+            $stmt->bindParam(1, $entity->taille, \PDO::PARAM_INT);
+            $stmt->execute();
+        }catch (\PDOException $e ){
+            // Gère les erreurs de la base de données
+            echo "Erreur : " . $e->getMessage();
+        }
 
     }
 
-    public function update($entity)
+    public static function update($entity)
     {
         // $requete = "call PanierPackage.UpdatePanier(:id, :idArticle, :idUser, :taille, :quantite)";
     }
 
-    public function delete($id)
+    public static function delete($id)
     {
+
     }
 
-    public function deleteArticle($id)
+    public static function deleteArticle($id)
     {
         $requete = "call PanierPackage.DeleteOneArticlePanier(:iduser, :idarticle)";
-        $stmt = $this->pdo->prepare($requete);
+        $stmt = self::$pdo->prepare($requete);
         $stmt->execute([
             'iduser' => $_SESSION['id'],
             'idarticle' => $id
         ]);
     }
 
-    public function getall()
+    public static function getall()
     {
+        try {
+            $requete = "CALL GetAllArticlePanier()";
+            $stmt = self::$pdo->prepare($requete);
+
+            $stmt->execute();
+            $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+            $articles = array();
+            foreach ($result as $article ){
+                $articles[] = new \backend\entity\PanierArticleEntity($article["id_Article"],$article["taille"],$article["quantite"]);
+            }
+            return new \backend\entity\PanierEntity($articles);
+            
+            
+        }catch (\PDOException $e ){
+            // Gère les erreurs de la base de données
+            echo "Erreur : " . $e->getMessage();
+        }
     }
 
-    public function getById(int $id): ?PanierEntity
+    public static function getById(int $id): ?\backend\entity\PanierEntity
     {
+        
     }
 
 
@@ -51,8 +79,28 @@ class DBPanier extends Connexion implements DAOInterface
      * @param int $id
      * @return array
      */
-    public function getPanierByUser(int $id): array
+    static public function getPanierByUser(int $id): array
     {
+        try {
+            $requete = "CALL GetArticlePanier(?)";
+            $stmt = self::$pdo->prepare($requete);
+            // Lie les paramètres d'entrée
+            $stmt->bindParam(1, $id, \PDO::PARAM_INT);
+
+            $stmt->execute();
+            $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+            $articles = array();
+            foreach ($result as $article ){
+                $articles[] = new \backend\entity\PanierArticleEntity($article["id_Article"],$article["taille"],$article["quantite"]);
+            }
+            return new \backend\entity\PanierEntity($articles);
+            
+            
+        }catch (\PDOException $e ){
+            // Gère les erreurs de la base de données
+            echo "Erreur : " . $e->getMessage();
+        }
     }
 
 
