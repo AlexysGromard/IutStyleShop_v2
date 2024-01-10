b21a7d884c977b0e5ac4be3c129a019e8962db67
lancelot <lancelot.jouault@gmail.com>
2024-01-10 07:32:03 -0500

    fusion

--- controller/DAO.php
+++ controller/DAO.php
@@ -9,16 +9,19 @@ class DAO{
 
 
     function index($className, $methodName, $params = array()) {
-        $this->dao = new $className();
+        // Créez une instance de la classe DAO
+        // $this->dao = new $className();
+    
         
-        // Vérifiez s'il y a des paramètres provenant du routeur
-        if (!empty($params)) {
-            // Appel de la méthode avec les paramètres provenant du routeur
-            $result = call_user_func_array(array($this->dao, $methodName), $params);
-        } else {
-            // Aucun paramètre provenant du routeur, appeler la méthode sans paramètres
-            $result = call_user_func(array($this->dao, $methodName));
-        }
+        // // Vérifiez s'il y a des paramètres provenant du routeur
+        // if (!empty($params)) {
+        //     // Appel de la méthode avec les paramètres provenant du routeur
+        //     $result = call_user_func_array(array($this->dao, $methodName), $params);
+        // } 
+        // // }elif (false == empty($params)) {
+        // //     // Aucun paramètre provenant du routeur, appeler la méthode sans paramètres
+        // //     $result = call_user_func(array($this->dao, $methodName));
+        // // }
         
         echo "jbefihbfejzbfjbezbjfkjebfkjeb";
         //require "frontend/".$className.".php";
