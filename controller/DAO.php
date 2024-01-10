62d338bd1b84f6ae48b5850e68aae0eb0e26f1c6
Bryan Levy <bryan.levy@etu.univ-nantes.fr>
2024-01-10 13:31:14 +0100

    Réglement DAO

--- controller/DAO.php
+++ controller/DAO.php
@@ -1,16 +1,20 @@
 <?php
 
-class DAO(){
+namespace controller;
 
-    private \Backend\DAO\DAOInterface $dao;
+class DAO{
 
+    private \backend\DAO\DAOInterface $dao;
 
+    function index(){
+        require "frontend/DAO/DBArticle.php";
+    }
 
     function get($className, $methodName, $params = array()){
         $this->dao = new $className();
         $result = call_user_func_array(array($this->dao, $methodName), $params[0]);
 
-        require "frontend/".$className.".php";
+        require "frontend/DAO".$className.".php";
     }
 
     function entity($className, $methodName, $params = array()){
