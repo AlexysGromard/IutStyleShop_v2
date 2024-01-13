74e6a042b7ac2dd35eca0fd6970133eedac8819e
E226772R <alexys.gromard@etu.univ-nantes.fr>
2024-01-13 19:58:16 +0100

    New footer

--- frontend/components/footer.php
+++ frontend/components/footer.php
@@ -1,42 +1,44 @@
 <footer>
-    <img id="footer-logo" src="/frontend/assets/icons/logo.svg" alt="IUTStyleShop logo">
-    <!-- Sitemap -->
-    <div class="footer-box-container">
-        <h3 class="footer-box-title">Plan du site</h3>
-        <div class="footer-box">
-            <ul>
-                <li><a href="/">Accueil</a></li>
-                <li><a href="/frontend/all-products/">Tous les articles</a></li>
-                <li><a href="/frontend/all-products/t-shirt/">T-Shirt</a></li>
-                <li><a href="/frontend/all-products/sweatshirt/">Sweat-shirt</a></li>
-            </ul>
-            <ul>
-                <li><a href="/frontend/all-products/sportswear/">Tenue de sport</a></li>
-                <li><a href="/frontend/all-products/accessories/">Accessoires</a></li>
-                <li><a href="/login">Connexion</a></li>
-                <li><a href="/register">Inscription</a></li>
-            </ul>
+    <div id="footer-container">
+        <img id="footer-logo" src="/frontend/assets/icons/logo.svg" alt="IUTStyleShop logo">
+        <!-- Sitemap -->
+        <div class="footer-box-container">
+            <h3 class="footer-box-title">Plan du site</h3>
+            <div class="footer-box">
+                <ul>
+                    <li><a href="/">Accueil</a></li>
+                    <li><a href="/frontend/all-products/">Tous les articles</a></li>
+                    <li><a href="/frontend/all-products/t-shirt/">T-Shirt</a></li>
+                    <li><a href="/frontend/all-products/sweatshirt/">Sweat-shirt</a></li>
+                </ul>
+                <ul>
+                    <li><a href="/frontend/all-products/sportswear/">Tenue de sport</a></li>
+                    <li><a href="/frontend/all-products/accessories/">Accessoires</a></li>
+                    <li><a href="/login">Connexion</a></li>
+                    <li><a href="/register">Inscription</a></li>
+                </ul>
+            </div>
         </div>
-    </div>
-    <!-- Credit -->
-    <div class="footer-box-container">
-        <h3 class="footer-box-title">Crédits</h3>
-        <div class="footer-box">
-            <ul>
-                <li><a href="/legal">Mentions légales</a></li>
-                <li><a href="#">Créateurs</a></li>
-            </ul>
+        <!-- Credit -->
+        <div class="footer-box-container">
+            <h3 class="footer-box-title">Crédits</h3>
+            <div class="footer-box">
+                <ul>
+                    <li><a href="/legal">Mentions légales</a></li>
+                    <li><a href="#">Créateurs</a></li>
+                </ul>
+            </div>
         </div>
-    </div>
-    <!-- Payment methods -->
-    <div class="footer-box-container">
-        <h3 class="footer-box-title">Moyens de paiement</h3>
-        <div class="footer-box">
-            <div id="payment-methods-images">
-                <img src="/frontend/assets/icons/visa.svg" alt="Visa logo">
-                <img src="/frontend/assets/icons/mastercard.svg" alt="Mastercard logo">
-                <img src="/frontend/assets/icons/card.svg" alt="CB logo"> 
-                <img src="/frontend/assets/icons/bitcoin.png" alt="BTC logo"> 
+        <!-- Payment methods -->
+        <div class="footer-box-container">
+            <h3 class="footer-box-title">Moyens de paiement</h3>
+            <div class="footer-box">
+                <div id="payment-methods-images">
+                    <img src="/frontend/assets/icons/visa.svg" alt="Visa logo">
+                    <img src="/frontend/assets/icons/mastercard.svg" alt="Mastercard logo">
+                    <img src="/frontend/assets/icons/card.svg" alt="CB logo"> 
+                    <img src="/frontend/assets/icons/bitcoin.png" alt="BTC logo"> 
+                </div>
             </div>
         </div>
     </div>
