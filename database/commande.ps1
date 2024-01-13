87a2ec0ef01ca89639dd39eae344584d74086b34
XIV <lancelot.jouault@outlook.com>
2024-01-13 20:55:54 +0100

    update commande.ps1

--- database/commande.ps1
+++ database/commande.ps1
@@ -20,7 +20,8 @@ if ($status -eq 'Running') {
 else {
     Write-Host "$service n'est pas actif." -ForegroundColor Yellow
 
-    Start-Service $service
+    
+    Start-Service -Name $service
     Start-Sleep -Seconds 5  # Attendre un moment pour que le service demarre
 
     $status = Get-Service -Name $service | Select-Object -ExpandProperty Status
@@ -42,7 +43,6 @@ Write-Host ""
 Write-Host "Execution des fichiers Database : " -ForegroundColor Magenta
 
 $sqlContent = Get-Content (Join-Path $scriptDirectory "mariadb\database.sql") | Out-String
-write-host $sqlContent
 mysql -h"$p_host" -P"$port" -u"$user" -p"$password" -e"$sqlContent" 
 
 Write-Host "    * $database" -ForegroundColor Yellow
