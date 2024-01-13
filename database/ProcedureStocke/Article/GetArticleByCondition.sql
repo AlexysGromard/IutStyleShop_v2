9260f918808ede2db8e9856d593229c9590796e3
IIBiniII <braline44600@gmail.com>
2024-01-13 19:47:26 +0100

    Arrangement du code de filtrage et de getArticleByCondition

--- database/ProcedureStocke/Article/GetArticleByCondition.sql
+++ database/ProcedureStocke/Article/GetArticleByCondition.sql
@@ -3,9 +3,9 @@ DELIMITER //
 
 
 -- Procédure pour récupérer les informations d'un article
-    CREATE OR REPLACE  PROCEDURE GetArticleByCondition(p_couleur VARCHAR(32), p_categorie VARCHAR(128),p_prix_min FLOAT,p_prix_max FLOAT)
+    CREATE OR REPLACE  PROCEDURE GetArticleByCondition(p_couleur VARCHAR(32), p_categorie VARCHAR(128),p_prix_min FLOAT,p_prix_max FLOAT,p_genre VARCHAR(16),p_promo TINYINT(1),p_disponible BOOLEAN)
     BEGIN
-        -- Logique pour récupérer les informations de l'article avec l'ID spécifié
-        SELECT * FROM Article WHERE couleur = p_couleur and category = p_categorie and prix >= p_prix_min and prix <= p_prix_max;
+            SELECT * FROM Article WHERE couleur = p_couleur and category = p_categorie and prix >= p_prix_min and prix <= p_prix_max and ((genre=p_genre) or (category not like 'accessoire' and p_genre like 'MF') ) AND ( (p_promo = 1 AND promo > 0) OR (p_promo = 0 AND promo = 0) ) and disponible = p_disponible;
+        
     END //
 DELIMITER ;
