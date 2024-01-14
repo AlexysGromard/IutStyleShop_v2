c5d6605b9a05adc7e1e75e5af2ae4b24133c5807
E226772R <alexys.gromard@etu.univ-nantes.fr>
2024-01-14 23:13:39 +0100

    Google analitics

--- frontend/common-includes.php
+++ frontend/common-includes.php
@@ -26,3 +26,26 @@
 
 <!-- Call cookies verification -->
 <?php include_once 'backend'.DIRECTORY_SEPARATOR.'cookies.php'; Cookies::verifyCookies() ?>
+<!-- Si la personne a accpeté les cookies -->
+<?php 
+// Si il n'y a pas de session active
+if (session_status() == PHP_SESSION_NONE) {
+  session_start();
+}
+if (isset($_SESSION['cookieAccept']) && $_SESSION['cookieAccept'] == true) {
+// On affiche le script de Google Analytics
+  echo "
+    <!-- Google tag (gtag.js) -->
+    <script async src=\"https://www.googletagmanager.com/gtag/js?id=G-SWLPL3D67R\"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+    
+        gtag('config', 'G-SWLPL3D67R');
+    </script>
+  ";
+}
+// Femer session
+session_write_close();
+?>
