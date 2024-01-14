34bb57dca8e799e4fc8ec2d5b541f7a49c2701ed
XIV <lancelot.jouault@outlook.com>
2024-01-14 20:46:53 +0100

    update commande.bash

--- controller/commentaire.php
+++ controller/commentaire.php
@@ -2,7 +2,12 @@
 
 namespace controller;
 
+
+use backend\entity\CommentaireEntity;
+
 class commentaire{
+
+    private \backend\DAO\DAOInterface $dao;
 
     /**
      * Cette fonction permet d'afficher le formulaire pour ajouter un commentaire
@@ -10,6 +15,28 @@ class commentaire{
     function index(){
         require "frontend/commentaire/commentaire.php";
 
+    }
+
+    function addCommentaire(){
+
+        
+
+        if (!empty($_POST['note'])) {
+            $commentaire = new CommentaireEntity($_SESSION['id'], $_POST['note'], htmlspecialchars($_POST['commentaire'])); 
+        
+            $this->dao = new \backend\DAO\DBCommentaire();
+
+            $this->dao::add($commentaire, $_POST['id_article']);
+
+            Location('Location: /article/visuel/'.$_POST['id_article']);
+        
+        
+        }
+        
+
+
+
+        Location('Location: /article/visuel/'.$_POST['id_article']);
     }
 
 }
