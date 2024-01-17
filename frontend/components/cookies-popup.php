d23a36751a3277c86e1fb43cfdcab1943a626da1
E226772R <alexys.gromard@etu.univ-nantes.fr>
2024-01-17 14:24:13 +0100

    Cookies

--- frontend/components/cookies-popup.php
+++ frontend/components/cookies-popup.php
@@ -3,10 +3,10 @@
         <div id="cookies-popup-left">
             <span id="cookies-popup-title">Gérer les cookies</span>
             <p id="cookies-popup-description">Nous utilisons des cookies pour améliorer votre expérience sur notre site. Ces cookies sont essentiels au bon fonctionnement du site et nous aident à comprendre comment vous l'utilisez. En continuant à naviguer, vous acceptez l'utilisation de ces cookies.</p>
-            <form method="POST" action="/backend/cookies.php" id="cookies-popup-btn-container">
-                <input type="submit" class="button" id="accept-cookies" name="accept-cookies" value="Accepter les cookies"/>
-                <input type="submit" class="button" id="deny-cookies" name="deny-cookies" value="Refuser les cookies" />
-            </form>
+            <div id="cookies-popup-btn-container">
+                <a class="button" id="accept-cookies" href="/cookies/acceptCookies">Accepter les cookies</a>
+                <a class="button" id="deny-cookies" href="/cookies/denyCookies">Refuser les cookies</a>
+            </div>
         </div>
         <div id="cookies-popup-right">
             <img src="/frontend/assets/icons/cookie.png" alt="Cookie image">
