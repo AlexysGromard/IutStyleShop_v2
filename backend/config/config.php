7a7f127392096ffa3378690057b0a637082ee031
E226772R <alexys.gromard@etu.univ-nantes.fr>
2024-02-11 12:41:50 +0100

    Add config file

---
+++ backend/config/config.php
@@ -0,0 +1,14 @@
+<?php
+    const DB_CONFIG = array(
+        "dbname" => "DBNAME",
+        "username" => "USERNAME",
+        "password" => "PASSWORD",
+        "host" => "HOST",
+        "type" => "TYPE",
+        "port" => "PORT" 
+    );
+    const MAIL_CONFIG = array(
+        "email" => "EMAIL",
+        "password" => "PASSWORD",
+    );
+?>
